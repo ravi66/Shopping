624657e57cb7df2c06cc60445ed02483567239e7
Ravi Kamat <ravitkamat@hotmail.com>
2024-04-21 14:25:14 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
